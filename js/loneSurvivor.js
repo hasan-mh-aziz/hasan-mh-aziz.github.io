1588f44a572af64b1cf1f468d997a7731ba35492
Mohammed Hasanul Aziz <hasan-mh-aziz@users.noreply.github.com>
2018-10-05 23:29:39 -0500

    Update loneSurvivor.js

--- js/loneSurvivor.js
+++ js/loneSurvivor.js
@@ -1,5 +1,5 @@
 $.ajaxPrefilter( function (options) {
-  if (options.crossDomain && jQuery.support.cors) {
+  if (options.crossDomain && jQuery.support.cors && 0) {
     var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
     options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
     //options.url = "http://cors.corsproxy.io/url=" + options.url;
