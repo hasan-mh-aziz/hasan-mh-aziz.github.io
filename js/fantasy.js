2c395854fbc449742593b4220f8450d4d4c04277
Mohammed Hasanul Aziz <hasan-mh-aziz@users.noreply.github.com>
2019-09-19 00:51:27 -0500

    Update fantasy.js

--- js/fantasy.js
+++ js/fantasy.js
@@ -2,7 +2,7 @@ $.ajaxPrefilter( function (options) {
   if (options.crossDomain && jQuery.support.cors) {
     var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
 //     options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-    options.url = "http://www.10coders.com/ffpbBoR/apis/enableCORS?corsUrl=" + options.url;
+    options.url = "https://www.10coders.com/ffpbBoR/apis/enableCORS?corsUrl=" + options.url;
     //options.url = "http://cors.corsproxy.io/url=" + options.url;
   }
 });
